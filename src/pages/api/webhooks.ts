f65bbffb0ec1eafd024bd770ec33ca92b64a3e4d
Guilherme Marques ALEST <guilherme.camara@alest.com.br>
2023-05-02 15:21:35 -0300

    feat: Add verification for other subscription events

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -23,8 +23,11 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-  'checkout.session.completed'
-])
+  'checkout.session.completed',
+  // 'customer.subscription.created',  not consider this event, because user can just subscribe by application
+  'customer.subscription.updated',
+  'customer.subscription.deleted'
+]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if(req.method === 'POST') {
@@ -44,12 +47,27 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if(relevantEvents.has(type)) {
       try {
         switch(type) {
+          // case 'customer.subscription.created':
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
+
+            break;
+          
           case 'checkout.session.completed':
             const checkoutSession = event.data.object as Stripe.Checkout.Session;
 
             await saveSubscription(
               checkoutSession.subscription.toString(),
-              checkoutSession.customer.toString()
+              checkoutSession.customer.toString(),
+              true
             );
 
             break;
