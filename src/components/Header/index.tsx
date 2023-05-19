71d2fa4a155a25df989bdf105d69ad53c2adde3f
GuilhermeMarques1 <g-marques@outlook.com>
2023-05-19 11:21:53 -0300

    feat: Add activeLink component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,35 @@
+import { useRouter } from "next/router";
 import { SignInButton } from "../SignInButton";
-import Link from "next/link";
+import { ActiveLink } from "../ActiveLink";
 
 import styles from "./styles.module.scss";
 
 export function Header() {  
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}> 
         <img src="/images/logo.svg" alt="logo do ig.news" />
         <nav>
-          <Link className={styles.active} href="/">
+          {
+            /*
+            <Link className={asPath === '/' ? styles.active : ''} href="/">
+              Home
+            </Link>
+             prefetch is automatically true
+            <Link className={asPath === '/posts' ? styles.active : ''} href="/posts"> 
+              Posts
+            </Link>
+            */
+          }
+
+          <ActiveLink activeClassName={styles.active} href='/'>
             Home
-          </Link>
-          {/* prefetch is automatically true */}
-          <Link href="/posts"> 
-            Posts
-          </Link>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href='/posts'>
+            Post
+          </ActiveLink>
         </nav>
         <SignInButton/>
       </div>
