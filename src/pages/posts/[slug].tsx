394de6625c37693adc7b0eacf6cbf37c312a2963
GuilhermeMarques1 <g-marques@outlook.com>
2023-06-06 01:39:55 -0300

    fix: Error for users not logged

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -43,7 +43,7 @@ export const getServerSideProps: GetServerSideProps = async ({req, res, params})
   const session = await getServerSession(req, res, authOptions);
   const { slug } = params;
 
-  if(!session.activeSubscription) {
+  if(!session?.activeSubscription) {
     return {
       redirect: {
         destination: "/",
