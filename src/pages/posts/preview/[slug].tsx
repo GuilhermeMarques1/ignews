aa822eb84ba7b6d523dc8fde34cc52f7aa64c795
GuilhermeMarques1 <g-marques@outlook.com>
2023-06-18 01:47:14 -0300

    refactor: Add revalidate arg to getStaticProps

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -86,6 +86,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   return {
     props: {
       post: post
-    }
+    },
+    // seconds to revalidate post
+    revalidate: 60 * 60, // 1 hour 
   };
 }
